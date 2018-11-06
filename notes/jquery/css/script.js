294f3aa739cec0322c8b9817a149a20f425e81ac
King Nguyen <hnguye78@gmu.edu>
2018-11-06 12:47:30 -0500

    putting project 3 under one file. oops:)
    got good feedback. Thank you to professor and clasmates.

---
+++ notes/jquery/css/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	demofunction();
+	bgchange();
+	$(window).resize(function() {
+
+	});
+;
+
+function demofunction() {
+	$('p').addclass('bigger');
+	$('p').click(function)(){
+		$('p').css('color', 'yellow');
+		console.log('add bigger');
+			console.log('yellow');
+
+	}
+
+
+}
