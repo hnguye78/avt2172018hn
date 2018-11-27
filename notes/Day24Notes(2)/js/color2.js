f79cc58d3a59dda6ce8199c70cb78d9114fbb707
King Nguyen <hnguye78@gmu.edu>
2018-11-27 10:27:11 -0500

    updating

--- notes/Day24Notes(2)/js/color2.js
+++ notes/Day24Notes(2)/js/color2.js
@@ -4,22 +4,23 @@ $(document).ready(function() {
 });
 
 function cartoonGuess() {
-	var cartoonArray = ["Nemo", "Dory", "Buzz Lightyear", "Woody", "Wall-e", "eva", "mike", "sullivan", "doris", "dash", "elastic girl",];
-	var imageArray = ["imgs/nemo.jpg", "imgs/dory.jpg", "imgs/buzzlight.jpeg", "imgs/woody.jpeg","imgs/wall_e.jpg","imgs/eva.png","imgs/mike_w.jpg","imgs/Sulley-right.png","imgs/doris.jpg","imgs/dash.jpg","imgs/elastigirl.jpg",];
-	var cartoon = Math.floor(Math.random() * cartoonArray.length);
-	var answerlower = cartoonArray[cartoon].toLowerCase();
-	var answerlower = cartoonArray[cartoon].toUpperCase();
-	var answer = cartoonArray[cartoon];
-	console.log(answer);
+	var cartoonArray = ["Nemo", "Dory", "Buzz Lightyear", "Woody", "Wall-e", "eva", "mike", "sullivan", "doris", "dash", "elastic girl"];
+	var imageArray = ["imgs/nemo.jpg", "imgs/dory.jpg", "imgs/buzzlight.jpeg", "imgs/woody.jpeg","imgs/wall_e.jpg","imgs/eva.png","imgs/mike_w.jpg","imgs/Sulley-right.png","imgs/doris.jpg","imgs/dash.jpg","imgs/elastigirl.jpg"];
+	var cartoon = Math.floor(Math.random() * imageArray.length);
+	var answerlower = cartoonArray[index].toLowerCase();
+	var answerlower = cartoonArray[index].toUpperCase();
+	var answer = cartoonArray[index];
+	console.log(answer);	
 	console.log(answerlower);
 	console.log(answerupper);
 	$('img').attr('src', imageArray[index]);
 	$('#submit').click(function() {
 		var guess = $('input').val();
 		console.log(guess);
-		if (guess == answer || guess == answerlower  || guess == answerupper) {
+		if (guess == answer || guess == answerlower || guess == answerupper) {
 			$('h1').text(guess + ' is right!');
 			$('input').val('');
+			$('img').attr('src', imageArray[index]);
 			$('.button p').text('Play Again?');
 			$('#submit').click(function() {
 				location.reload(true);
